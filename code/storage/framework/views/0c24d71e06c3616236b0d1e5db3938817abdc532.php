7d78a4a0b6975cdd5f0e40e52123f1ea1b79142f
rachmad <rahmadfirmansyah59@gmail.com>
2023-02-24 08:57:06 +0700

    23 februari 2023

---
+++ code/storage/framework/views/0c24d71e06c3616236b0d1e5db3938817abdc532.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>PT. SULAWESI TAMBANG UTAMA</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo e(asset('assets/techie/img/favicon/apple-touch-icon.png')); ?>">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo e(asset('assets/techie/img/favicon/favicon-32x32.png')); ?>">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo e(asset('assets/techie/img/favicon/favicon-16x16.png')); ?>">
+  <link rel="manifest" href="<?php echo e(asset('assets/techie/img/favicon/site.webmanifest')); ?>">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="<?php echo e(asset('assets/techie/vendor/aos/aos.css')); ?>" rel="stylesheet">
+  <link href="<?php echo e(asset('assets/techie/vendor/bootstrap/css/bootstrap.min.css')); ?>" rel="stylesheet">
+  <link href="<?php echo e(asset('assets/techie/vendor/bootstrap-icons/bootstrap-icons.css')); ?>" rel="stylesheet">
+  <link href="<?php echo e(asset('assets/techie/vendor/boxicons/css/boxicons.min.css')); ?>" rel="stylesheet">
+  <link href="<?php echo e(asset('assets/techie/vendor/glightbox/css/glightbox.min.css')); ?>" rel="stylesheet">
+  <link href="<?php echo e(asset('assets\techie\vendor\owl-carousel\owl.carousel.min.css')); ?>" rel="stylesheet">
+  <link href="<?php echo e(asset('assets/techie/vendor/swiper/swiper-bundle.min.css')); ?>" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="<?php echo e(asset('assets/techie/css/style.css')); ?>" rel="stylesheet">
+
+  <?php echo $__env->yieldContent('css'); ?>
+
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center justify-content-between">
+      <a href="#" class="logo"><img src="<?php echo e(asset('assets/techie/img/logo-1.png')); ?>" alt="" class="img-fluid"></a>
+      <h5 class="logo"><a href="#top">PT. SULAWESI TAMBANG UTAMA</a></h5>
+      <!-- Uncomment below if you prefer to use an image logo -->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#tentang-kami">Tentang Kami</a></li>
+          <li><a class="nav-link scrollto" href="#dasar_hukum">Dasar Hukum</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Portofolio</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+          <li><a class="nav-link scrollto" href="#our_patner">Our Partners</a></li>
+          <li><a class="getstarted scrollto" href="<?php echo e(asset('assets/file/compro.pdf')); ?>" download>Company Profile</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <?php echo $__env->yieldContent('content'); ?>
+
+  <a href="https://api.whatsapp.com/send?phone=6281233362166&text=text=HALO%20INFO%20TENTAN%20PT%20SULAWESI%20TAMBANG%20UTAMA." class="float" target="_blank">
+    <i class="bi bi-whatsapp"></i>
+  </a>
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6 footer-contact">
+            <img src="<?php echo e(asset('assets/techie/img/logo-1.png')); ?>" class="img-fluid" width="75px" alt="">
+            <h3 class="mt-3">PT. SULAWESI TAMBANG UTAMA</h3>
+            <p>
+              <strong>Phone:</strong> +62 812 3336 2166<br>
+              <strong>Email:</strong> sulawesi.tambangutama@gmail.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-links">
+            <div class="d-flex justify-content-start">
+              <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#home">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#tentang-kami">Tentang Kami</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#dasar-hukum">Dasar Hukum</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#portofolio">Portofolio</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#contact-us">Contact Us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="<?php echo e(asset('assets/file/compro.pdf')); ?>" download class="getstarted">Download Company Profile</a></li>
+            </ul>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-12 footer-newsletter">
+            <h4>Kantor Pusat</h4>
+            <div class="d-flex mb-3">
+              <i class="bi bi-geo-alt-fill" style="margin-top: 1px"></i>
+              <p class="mx-1">Ruko Dutamas Fatmawati A1/38 Desa<br> Kelurahan Cipete Utara, Kec Kebayoran Baru, Kota Adm. Jakarta Selatan, Propinsi DKI Jakarta - 12150</p>
+            </div>
+            <h4>Lokasi Usaha</h4>
+            <div class="d-flex">
+              <i class="bi bi-geo-alt-fill" style="margin-top: 1px"></i>
+              <p class="mx-1">Desa Ganda-Ganda<br> Dusun/Kelurahan Bahoue, Kec Petasia, Kab Morowali Utara, Sulawesi Tengah - 94971</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright-wrap d-md-flex py-4">
+        <div class="me-md-auto text-center text-md-start">
+          <div class="copyright">
+            &copy; Copyright 2023 <strong><span>PT. STU</span></strong>.
+          </div>
+          <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/techie-free-skin-bootstrap-3/ -->
+            </div>
+        </div>
+        <div class="social-links text-center text-md-right pt-3 pt-md-0">
+          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
+      </div>
+
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="<?php echo e(asset('assets/techie/vendor/purecounter/purecounter_vanilla.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/techie/js/jquery-3.6.3.min.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/techie/vendor/aos/aos.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/techie/vendor/bootstrap/js/bootstrap.bundle.min.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/techie/vendor/glightbox/js/glightbox.min.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/techie/vendor/isotope-layout/isotope.pkgd.min.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/techie/vendor/swiper/swiper-bundle.min.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets\techie\vendor\owl-carousel\owl.carousel.min.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/techie/vendor/php-email-form/validate.js')); ?>"></script>
+
+  <!-- Template Main JS File -->
+  <script src="<?php echo e(asset('assets/techie/js/main.js')); ?>"></script>
+
+  <?php echo $__env->yieldContent('script'); ?>
+
+</body>
+
+</html><?php /**PATH /Users/rachmad/Sites/stu-web/code/resources/views/front/layouts/layouts.blade.php ENDPATH**/ ?>
